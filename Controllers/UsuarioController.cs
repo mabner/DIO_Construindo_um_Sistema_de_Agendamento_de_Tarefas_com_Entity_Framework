1516fd846466d3478347dea4afbbda3de56d4aa3
Marcos Leite <m.abner@live.co.uk>
2023-01-09 12:49:03 -0300

    Apresentar method implemented

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,8 +9,12 @@ namespace TrilhaApiDesafio.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class DataHoraController : ControllerBase
+	public class UsuarioController : ControllerBase
 	{
+		/*
+		* Defines the method used in the URL to access
+		* the resource. E.g. DataHora/ObterDataHoraAtual
+		*/
 		[HttpGet("ObterDataHoraAtual")]
 		public IActionResult ObterDataHora()
 		{
@@ -19,6 +24,13 @@ namespace TrilhaApiDesafio.Controllers
 				Hora = DateTime.Now.ToShortTimeString()
 			};
 			return Ok(obj);
+		}
+
+		[HttpGet("Apresentar/{name}")]
+		public IActionResult Apresentar(string name)
+		{
+			var message = $"Olá {name}, seja bem vindo!";
+			return Ok(new { message });
 		}
 	}
 }
