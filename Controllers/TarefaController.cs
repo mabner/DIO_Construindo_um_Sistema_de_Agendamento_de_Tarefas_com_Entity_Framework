e160ef5ed41ebbdd1a652a14d6817502ad5d674d
Marcos Leite <m.abner@live.co.uk>
2023-01-15 23:12:17 -0300

    implements find by title

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Intrinsics.X86;
 using Microsoft.AspNetCore.Mvc;
 using TrilhaApiDesafio.Context;
 using TrilhaApiDesafio.Models;
@@ -46,9 +47,8 @@ namespace TrilhaApiDesafio.Controllers
 		[HttpGet("ObterPorTitulo")]
 		public IActionResult ObterPorTitulo(string titulo)
 		{
-			// TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
-			// Dica: Usar como exemplo o endpoint ObterPorData
-			return Ok();
+			var tarefas = _context.Tarefas.Where(x => x.Titulo.Contains(titulo));
+			return Ok(tarefas);
 		}
 
 		[HttpGet("ObterPorData")]
